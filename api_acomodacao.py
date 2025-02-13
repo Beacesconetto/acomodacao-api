0fc900ee01b9d08507767f31b6d3806dfa9d1c62
Beacesconetto <beacesconetto203@hotmail.com>
2025-02-13 17:08:09 -0300

    feat: database connection

--- api_acomodacao.py
+++ api_acomodacao.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI, HTTPException, Query
 import json
+from dotenv import load_dotenv
+import os
+import psycopg2
+from pydantic import BaseModel
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
+
+class Accommodation(BaseModel):
+    name: str
+    image: str
+    location: str
+    price: float
+
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
+)
+
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS accommodations (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        image TEXT NOT NULL,
+        location TEXT NOT NULL,
+        price REAL NOT NULL
+    )
+""")
+conn.commit()
 
 DATA_FILE = "acomodacoes.json"
 
@@ -17,20 +51,63 @@ def save_files(dados):
         json.dump(dados, file, indent=4, ensure_ascii=False)
 
 @app.get("/acomodacoes")
-def  list_all_accommodations():
-    return load_files()
+def list_all_accommodations():
+    try:
+        cursor.execute("SELECT id, name, image, location, price FROM accommodations")
+        acomodacoes = cursor.fetchall()
+
+        result = [
+            {"id": a[0], "name": a[1], "image": a[2],"location": a[3], "price": a[4]}
+            for a in acomodacoes
+        ]
+
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar do banco: {str(e)}")
+
 
 @app.get("/acomodacoes/filtrar")
-def filter_accommodations_by_city(cidade: str = Query(..., description="Filtrar por cidade")):
+def filter_accommodations_by_location(cidade: str = Query(..., description="Filtrar por cidade")):
+    '''
+    Essa função não será usada no front-end para que ele não faça 
+    várias consultas ao banco de dados. Para filtrar as cidades o front irá utilizar
+    a rota /acomodacoes
+    '''
     dados = load_files()
     filtradas = [a for a in dados if cidade.lower() in a["cidade"].lower()]
     return filtradas
 
 @app.get("/acomodacoes/{id}")
 def get_accommodations(id: int):
-    dados = load_files()
-    for a in dados:
-        if a["id"] == id:
-            return a
-    raise HTTPException(status_code=404, detail="Acomodação não encontrada")
+    try:
+        cursor.execute("SELECT id, name, image, location, price FROM accommodations WHERE id = %s", (id,))
+        accommodation = cursor.fetchone()
 
+        if not accommodation:
+            raise HTTPException(status_code=404, detail="Acomodação não encontrada")
+
+        return {
+            "id": accommodation[0],
+            "name": accommodation[1],
+            "image": accommodation[2],
+            "location": accommodation[3],
+            "price": accommodation[4]
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar do banco: {str(e)}")
+
+@app.post("/acomodacoes")
+def create_accommodations(accommodation: Accommodation):
+    print(accommodation)
+    try:
+        cursor.execute(
+            "INSERT INTO accommodations (name, image, location, price) VALUES (%s, %s, %s, %s) RETURNING id",
+            (accommodation.name, accommodation.image, accommodation.location, accommodation.price)
+        )
+        new_id = cursor.fetchone()[0]
+        conn.commit()
+        return {"mensagem": "Acomodação criada com sucesso", "id": new_id, "dados": accommodation}
+    except Exception as e:
+        print(e)
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=f"Erro ao inserir no banco: {str(e)}")
