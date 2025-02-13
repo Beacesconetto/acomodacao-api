6e0f541162edc63d970db4afe55d33d000bc1745
Beacesconetto <beacesconetto203@hotmail.com>
2025-02-13 14:45:17 -0300

    fix: change functions name

--- api_acomodacao.py
+++ api_acomodacao.py
@@ -5,30 +5,30 @@ app = FastAPI()
 
 DATA_FILE = "acomodacoes.json"
 
-def carregar_dados():
+def load_files():
     try:
         with open(DATA_FILE, "r", encoding="utf-8") as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         return []
 
-def salvar_dados(dados):
+def save_files(dados):
     with open(DATA_FILE, "w", encoding="utf-8") as file:
         json.dump(dados, file, indent=4, ensure_ascii=False)
 
 @app.get("/acomodacoes")
-def listar_todas_acomodacoes():
-    return carregar_dados()
+def  list_all_accommodations():
+    return load_files()
 
 @app.get("/acomodacoes/filtrar")
-def filtrar_acomodacoes_por_cidade(cidade: str = Query(..., description="Filtrar por cidade")):
-    dados = carregar_dados()
+def filter_accommodations_by_city(cidade: str = Query(..., description="Filtrar por cidade")):
+    dados = load_files()
     filtradas = [a for a in dados if cidade.lower() in a["cidade"].lower()]
     return filtradas
 
 @app.get("/acomodacoes/{id}")
-def obter_acomodacao(id: int):
-    dados = carregar_dados()
+def get_accommodations(id: int):
+    dados = load_files()
     for a in dados:
         if a["id"] == id:
             return a
